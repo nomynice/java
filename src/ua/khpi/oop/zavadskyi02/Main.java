8f42be2a96e5443229e08fad3aadabab13aa49fc
nomynice <imnomynice@gmail.com>
2020-12-17 23:01:48 +0200

    labs/t

--- src/ua/khpi/oop/zavadskyi02/Main.java
+++ src/ua/khpi/oop/zavadskyi02/Main.java
@@ -1,40 +1,37 @@
 package ua.khpi.oop.zavadskyi02;
 
+import java.util.Random;
 public class Main {
 	  public static void main(String[] args) {
 	     
 	    double rand_num;
 	    int x;
+	    System.out.println("|    Значение     |    Число  \t|");
+	    System.out.println("|_________________|_____________|");
 	    for(int i=0;i<10;i++)
+	      
 	    {
 	      rand_num = Math.random()*10;
 	      x =  (int)rand_num;
 	      task(x);
-	      System.out.println("____________________________");
+	      System.out.println("|_________________|_____________|");
 	    }
 
 	      }
 	  
 	  public static void task(int x)
 	  { 
-	    System.out.println("Число :"+x ) ;
+	    int cop = x; 
 	        for ( int i=2; i < x; i++) {
 	                if ( x % i == 0) {
-	                        System.out.println("Это число не является постым.");
+	                        System.out.println("|Не простое\t  |\t"+cop+" \t|");
 	                        return;
 	                }      
 	        }
-	        System.out.println("Это число простое"); 
+	        System.out.println("|Простое\t  |\t"+cop+" \t|"); 
 	        return;
 	  }
 	  
 	  
 	  
-	
-	
-	
-	
-	
-	
-	
-}
+	}
